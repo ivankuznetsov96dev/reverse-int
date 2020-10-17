331649420d38bee9c86a19fc4d27250aedbdc881
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2020-10-17 23:07:10 +0300

    feat: reverse int task is compleated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let param = ('' + n).split('').reverse();
+  if (param[param.length - 1] === '-') {
+    return param.join('').slice(0, -1);
+  } else return param.join('');
 }
